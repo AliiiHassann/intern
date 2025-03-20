c79661fe4fb21b4eb5cc5b7a7e7d32dc6193f1de
AliiiHassann <144592141+AliiiHassann@users.noreply.github.com>
2025-03-21 00:26:23 +0200

    fixing

--- src/components/PrivateSource.tsx
+++ src/components/PrivateSource.tsx
@@ -1,14 +1,14 @@
-import React, { ReactNode } from "react";
-import { Navigate } from "react-router-dom";
+// import React, { ReactNode } from "react";
+// import { Navigate } from "react-router-dom";
 
-// Define Props Type
-interface PrivateRouteProps {
-  children: ReactNode;
-}
+// // Define Props Type
+// interface PrivateRouteProps {
+//   children: ReactNode;
+// }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-  const token = localStorage.getItem("token");
-  return token ? children : <Navigate to='/' />;
-};
+// const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+//   const token = localStorage.getItem("token");
+//   return token ? children : <Navigate to='/' />;
+// };
 
-export default PrivateRoute;
+// export default PrivateRoute;
